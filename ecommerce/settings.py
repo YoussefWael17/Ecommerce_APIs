ea816fe9e3411384f8d9aa3b9feca96225ed051a
YoussefWael17 <youssef.wael9906@gmail.com>
2025-04-28 16:46:40 +0300

    Add + settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -172,3 +172,10 @@ AUTH_USER_MODEL = 'api.CustomUser'
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET")
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
